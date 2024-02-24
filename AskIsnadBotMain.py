e824152f3a67a393d619b231447133051a8f1251
Mohamed Mansour <45554974+mohamad-mansour@users.noreply.github.com>
2024-02-24 22:28:06 +0300

    Update AskIsnadBotMain.py

--- AskIsnadBotMain.py
+++ AskIsnadBotMain.py
@@ -295,7 +295,7 @@ def button_click(update: Update, context: CallbackContext) -> None:
 def main() -> None:
     """Run the bot."""
     # Create the Updater and pass it your bot's token
-    updater = Updater("6845309288:AAHMogxz4h1vT4PxNXC_SX6U7Dt-prhEyIA")
+    updater = Updater("6845309288:AAHFv1Gz-SVlbWbJvd-XbXKM6QqOzDV9sK4")
 
     # Get the dispatcher to register handlers
     dispatcher = updater.dispatcher
